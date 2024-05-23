25c2d1d84d57142e1ae60e162c3539969d540c0c
Tamilarasan <Tamilarasanpandiyan@gmail.com>
2024-05-23 20:42:19 +0530

    delete

--- API/server.js
+++ API/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors(
     {
     origin: ["https://crud-frontend-hazel.vercel.app"],
-    methods: ["POST", "GET" ,"PUT" , "DELLETE"],
+    methods: ["POST", "GET" ,"PUT" , "DELETE"],
     credentials: true
 }
 ));
